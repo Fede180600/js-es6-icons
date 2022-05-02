55156012ee47c8a8aa966052614c087f5ac6c58e
Fede180600 <fede180600@libero.it>
2022-05-02 22:23:58 +0200

    bonus done

--- js/script.js
+++ js/script.js
@@ -156,27 +156,51 @@ const icons = [
 //     `
 // })
 
+
+const itemsContainer = document.getElementById('items-container');
+
+printIcons(icons);
+
+function printIcons(items) {
+    itemsContainer.innerHTML = "";
+    //Imposto ciclo forEach per scorrere tutti gli Objects della array
+    items.forEach((element) => {
+        const newItem = document.createElement('div');
+        newItem.classList.add('item');
+        itemsContainer.append(newItem);
+        newItem.innerHTML = `
+            
+            <div class="item-icon">
+                <span><i class="${element.family} ${element.prefix}${element.name} ${element.color}"></i></span>
+            </div>
+            <div class="item-name">
+                <p>${element.name}</p>
+            </div>
+            
+        `
+    })
+}
+
 //  BONUS
 // Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti
-const myFilter = document.getElementById('filter').value;
+let myFilter = document.getElementById('filter');
 myFilter.addEventListener("click", function() {
-    console.log(myFilter.value);
-    
-})
-// const itemsContainer = document.getElementById('items-container');
-// //Imposto ciclo forEach per scorrere tutti gli Objects della array
-// icons.forEach((element) => {
-//     const newItem = document.createElement('div');
-//     newItem.classList.add('item');
-//     itemsContainer.append(newItem);
-//     newItem.innerHTML = `
-        
-//         <div class="item-icon">
-//             <span><i class="${element.family} ${element.prefix}${element.name} ${element.color}"></i></span>
-//         </div>
-//         <div class="item-name">
-//             <p>${element.name}</p>
-//         </div>
-        
-//     `
-// })
+    let filteredIcons = icons.filter((element) => {
+        if (myFilter.value === "all") {
+            return element.type !== "all";
+        } else if (myFilter.value === "animal"){
+            return element.type === "animal";
+        } else if (myFilter.value === "vegetable") {
+            return element.type === "vegetable";
+        } else if (myFilter.value === "user") {
+            return element.type === "user";
+        }
+    })
+
+    // stampare solo icone filtrate
+    printIcons(filteredIcons);
+
+});
+
+
+
